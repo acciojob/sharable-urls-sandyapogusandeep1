35318521648304d311d0f3258f2b93b594cfeff2
sandyapogusandeep1 <125143336+sandyapogusandeep1@users.noreply.github.com>
2025-02-09 23:13:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-// your code here
+document.getElementById("button").addEventListener("click", function() {
+            const name = encodeURIComponent(document.getElementById("name").value);
+            const year = encodeURIComponent(document.getElementById("year").value);
+            const baseUrl = "https://localhost:8080/";
+            const queryString = `?name=${name}&year=${year}`;
+            document.getElementById("url").textContent = baseUrl + queryString;
+        });
